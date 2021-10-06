16f0cd647cfc4477129a808013ad1e718652ae84
Sumith Damodaran <sumithpd@gmail.com>
2021-10-06 12:13:00 +0100

    added asset and updated main_screen

--- book_tracker/lib/screens/main_screen.dart
+++ book_tracker/lib/screens/main_screen.dart
@@ -5,10 +5,27 @@ class MainScreenPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Material(
-      child: Center(
-        child: Text('Main Screen'),
+    return Scaffold(
+        appBar: AppBar(
+      backgroundColor: Colors.white24,
+      elevation: 0.0,
+      toolbarHeight: 77,
+      centerTitle: false,
+      title: Row(
+        children: [
+          Image.asset(
+            'assets/images/Icon-76.png',
+            scale: 2,
+          ),
+          Text(
+            'A.Reader',
+            style: Theme.of(context)
+                .textTheme
+                .headline6!
+                .copyWith(color: Colors.redAccent, fontWeight: FontWeight.bold),
+          ),
+        ],
       ),
-    );
+    ));
   }
 }
